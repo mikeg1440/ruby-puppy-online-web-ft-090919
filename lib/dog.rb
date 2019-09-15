87ae616259f999c9eb1c0c24dd5cdf8932e4ccbc
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-15 22:53:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,5 +9,20 @@ class Dog
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog
+    end
+
+  end
+
 
 end
